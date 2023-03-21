8935fb254f42c163a76055c7665d20790bf3e2e7
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-03-21 09:41:24 -0300

    organizando arquivos

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logowebglobal.svg" width="125" height="125" /> -->
+    <img class="h-200 w-200" src="https://webglobal.com.br/carreiras/wp-content/uploads/2022/09/logo-webglobal.png" alt="">
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">Contato</RouterLink>
+      </nav>
+     
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 30vh;
+  z-index: 2;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+text-decoration: none;
+color: rgb(10, 54, 189);
+transition: 0.4s;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -0rem;
+    font-size: 3rem;
+
+    padding: 2rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
+<!--  -->
