926ddfb064978f539341a51472fde188e05b6cc0
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2023-03-24 15:45:19 -0300

    Update TheWelcome.vue
    adicionar rota através e escondendo no bottom através do vshow

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -2,24 +2,24 @@
 export default {
   data() {
     return {
-      image: { background: "linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url(/src/assets/fundomundodedados.jpg)" }
+      isOpen:false,
     };
+  },
+  
+  // data() {
+  //   return {
+  //     image: { background: "linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url(/src/assets/fundomundodedados.jpg)" }
+  //   };
+  // },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    }
   }
-};
-// export default {
-//   data() {
-//     return {
-//       fundovisivel: false
-//     }
-//   },
-//   methods: {
-//     handleclick() {
-//       this.fundovisivel = !this.fundovisivel;
-//     }
-//   }
-// }
-</script>
 
+}; 
+
+</script>
 
 <template >
   <div id="teste-backgroudcolor">
@@ -35,14 +35,11 @@ export default {
 
                 <div class="relative inline-block text-left">
                   <div>
-                    <button type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="false" aria-haspopup="false">
+                    <button @click.stop.prevent= toggle type="button" class="inline-flex w-full justify-center text-lg font-semibold leading-6 text-gray-900 text-gray-900  ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="false" aria-haspopup="false">
                       Soluções
-                      <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                      </svg>
                     </button>
                   </div>
-                  <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                  <div v-show="isOpen" class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                     <div class="py-1" role="none">
                       <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0"><RouterLink to="/solucoes">Fabricantes</RouterLink></a>
                       <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0"><RouterLink to="/solucoes">Lojistas</RouterLink></a>
@@ -52,8 +49,6 @@ export default {
                 </div>
 
                 
-
-
                 <a href="#" class="text-lg font-semibold leading-6 text-gray-900"><RouterLink to="/clientes">Clientes</RouterLink></a>
 
                 <a href="#" class="text-lg font-semibold leading-6 text-gray-900"><RouterLink to="/conteudos">Conteúdos</RouterLink></a>
